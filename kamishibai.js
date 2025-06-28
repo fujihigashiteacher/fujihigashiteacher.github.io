1c446776f4e1373bda8e46d5b8456f6353de2642
fujihigashiteacher <45133579+fujihigashiteacher@users.noreply.github.com>
2025-06-28 13:30:45 -0700

    Update kamishibai.js

--- kamishibai.js
+++ kamishibai.js
@@ -106,7 +106,7 @@ function nextSlide(){
 class Slide{    
     constructor(art, sound = null){
         this.art = art;
-        this.art.resize(width-MARGIN);
+        this.art.resize(width-20);
         this.sound = sound;
         this.w = 1920;
         this.h = 1080;
